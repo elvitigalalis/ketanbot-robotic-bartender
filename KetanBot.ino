7deccaa72ab61960b1d52a80121fe71927a9f59a
9016Lysol <elvitigalalis@gmail.com>
2023-04-16 23:58:19 -0400

    declaring variables

--- KetanBot.ino
+++ KetanBot.ino
@@ -4,7 +4,6 @@ Coding and Web Design Officer Application
 4/5-4/16
 */
 
-
 #include <Adafruit_MotorShield.h>
 
 // Create the motor shield object with the default I2C address
@@ -12,23 +11,45 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Connect a stepper motor with 200 steps per revolution (1.8 degree)
 // to motor port #1 (M1 and M2)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(48, 1);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(48, 2);
+const int limitSwitchPin = 2;
+const int greenPin = 10;
+const int redPin = 9;
+int bluePin = A1;
 
+int numberCollector;
+byte serialNumber;
+const int opticCount = 4;
+const int parameterCount = 3;
+const int parameterSize = 2;
 
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  while (!Serial);
-  Serial.println("Stepper test!");
+int drinkMatrix[opticCount][parameterCount] = {};
 
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-    Serial.println("Could not find Motor Shield. Check wiring.");
-    while (1);
-  }
-  Serial.println("Motor Shield found.");
+int prevButtonState = 0;
+int currentButtonState = 0;
 
-  myMotor->setSpeed(10);  // 10 rpm
+bool contactSwitchFound = false;
+bool drinkRequested = false;
+
+void setup()
+{
+    Serial.begin(9600); 
+    while (!Serial)
+        ;
+    Serial.println("Stepper test!");
+
+    if (!AFMS.begin())
+    { 
+        Serial.println("Could not find Motor Shield. Check wiring.");
+        while (1)
+            ;
+    }
+    Serial.println("Motor Shield found.");
+
+    myMotor->setSpeed(10); 
 }
 
-void loop() {
-  
+void loop()
+{
 }
